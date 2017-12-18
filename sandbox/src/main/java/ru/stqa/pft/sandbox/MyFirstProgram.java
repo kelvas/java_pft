f64817f166c2d260716c49b56ca371a7f19c7b71
kelvas <kuzminsun1@gmail.com>
2017-12-18 15:31:11 +0300

    Некоторый изменения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,6 +5,10 @@ public class MyFirstProgram {
   public static void main(String[] args) {
 
     System.out.println("Hello, World!");
+
+    Point p1 = new Point(1,3);
+    Point p2 = new Point(3,4);
+    System.out.println("Дистанция между точками равна = " + p1.distance(p2));
   }
 
 }

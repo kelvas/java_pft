f2688ce0486befb580c505eac3eef025dd5260aa
kelvas <kuzminsan1@gmail.com>
2017-12-18 07:41:48 +0300

    Создан класс Point

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+
+  double x;
+  double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public double distance (Point p) {
+    double dx = p.x - this.x;
+    double dy = p.y - this.y;
+    return  Math.sqrt(dx*dx + dy*dy);
+  }
+
+}

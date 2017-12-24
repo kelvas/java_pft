d799d1052f0a478271a65b77463c92270579c927
kelvas <kuzminsan1@gmail.com>
2017-12-24 13:43:15 +0300

    Улучшили читаемость кода, выделии вспомогательные методы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestGroupCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestGroupCreation.java
@@ -3,76 +3,86 @@ package ru.stqa.pft.addressbook;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
-
 
 public class TestGroupCreation {
+  //FirefoxDriver wd;
+  ChromeDriver wd;
 
-    //FirefoxDriver wd;   // Firefox
-    ChromeDriver wd;    // Chrome
+  @BeforeMethod // Метод инициализации фикстуры
+  public void setUp() throws Exception {
+    //System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+    //wd = new FirefoxDriver();
+    System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("admin", "secret");    // вход в систему с любым пользователем и паролем
+  }
 
-    // Метод инициализации фикстуры
-    @BeforeMethod
-    public void setUp() throws Exception {
-        //System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");    // Firefox
-        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");    // Chrome
-        //FirefoxDriver wd = new FirefoxDriver(); // Firefox
-        wd = new ChromeDriver();    // Chrome
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/group.php");
-        login();
+  private void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  @Test
+  public void testGroupCreation() {
+    gotoGroupPage();        // перейти на страницу списка групп
+    initGroupCreation();    // инициализировать создание новой группы
+    fillGroupForm(new GroupDate("test1", "test2", "test3")); // заполнить форму разными данными
+    submitGroupCreation();  // подтвердить создание группы
+    returnToGroupPage();    // вернуться на страницу со списком групп
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupDate groupDate) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupDate.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupDate.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupDate.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
+
+
+  @AfterMethod  // Метод завершения фикстуры
+  public void tearDown() {
+    wd.quit();
+  }
+
+  //public static boolean isAlertPresent(FirefoxDriver wd) {
+  public static boolean isAlertPresent(ChromeDriver wd) {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
     }
-
-    private void login() {
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-    }
-
-    @Test
-    public void testGroupCreation() {
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("new")).click();
-        wd.findElement(By.name("group_name")).click();
-        wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("test1");
-        wd.findElement(By.name("group_header")).click();
-        wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("test2");
-        wd.findElement(By.name("group_footer")).click();
-        wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("test3");
-        wd.findElement(By.name("submit")).click();
-        wd.findElement(By.linkText("group page")).click();
-    }
-
-    // Метод завершения фикстуры
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-
-    // public static boolean isAlertPresent(FirefoxDriver wd) {     // Firefox
-    public static boolean isAlertPresent(ChromeDriver wd) {     // Chrome
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UntitledTestCase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UntitledTestCase.java
@@ -1,74 +1,47 @@
 package ru.stqa.pft.addressbook;
 
+import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-
 import org.testng.annotations.*;
-
 import static org.testng.Assert.*;
-
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class TestUntitled {
+public class UntitledTestCase {
   private WebDriver driver;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
-    driver = new ChromeDriver();
+    System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
+    driver = new FirefoxDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    login("admin", "secret");
-
   }
-
-  private void login(String username, String password) {
-    driver.get("http://localhost/addressbook/group.php");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
 
   @Test
   public void testUntitledTestCase() throws Exception {
-    gotoGroupPage();
-    initGroupCreation();
-    fillGroupForm(new GroupData("test5", "test6", "test7"));
-    submitGroupCreation();
-    returnToGroupPage();
-  }
-
-  private void returnToGroupPage() {
-    driver.findElement(By.linkText("group page")).click();
-  }
-
-  private void submitGroupCreation() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  private void fillGroupForm(GroupData groupData) {
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("user")).click();
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
     driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    driver.findElement(By.name("group_name")).sendKeys("test1");
     driver.findElement(By.name("group_header")).click();
     driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    driver.findElement(By.name("group_header")).sendKeys("test2");
     driver.findElement(By.name("group_footer")).click();
     driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initGroupCreation() {
-    driver.findElement(By.name("new")).click();
-  }
-
-  private void gotoGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.name("group_footer")).sendKeys("test3");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("group page")).click();
   }
 
   @AfterClass(alwaysRun = true)

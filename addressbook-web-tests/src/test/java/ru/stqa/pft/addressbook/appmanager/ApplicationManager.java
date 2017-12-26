9a79d5da4ab97a61abb7f00654e27b4e78ca8421
kelvas <kuzminsan1@gmail.com>
2017-12-27 02:24:35 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,29 +1,36 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.fail;
 
 public class ApplicationManager {
-  //FirefoxDriver driver;
-  ChromeDriver driver;
+  WebDriver driver;
 
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() throws Exception {
-    //System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver.exe");
-    //driver = new FirefoxDriver();
-    System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
-    driver = new ChromeDriver();
+    if (browser == BrowserType.FIREFOX) {
+      driver = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME){
+      driver = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      driver = new InternetExplorerDriver();
+    }
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(driver);
---
+++ sandbox/src/test/java/rus/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,26 @@
+package rus.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Equation;
+
+public class EquationTests {
+
+  @Test
+  public void test0() {
+    Equation e = new Equation(1, 1, 1);
+    Assert.assertEquals(e.rootNamber(), 0);
+  }
+
+  @Test
+  public void test1() {
+    Equation e = new Equation(1, 2, 1);
+    Assert.assertEquals(e.rootNamber(), 1);
+  }
+
+  @Test
+  public void test2() {
+    Equation e = new Equation(1, 5, 6);
+    Assert.assertEquals(e.rootNamber(), 2);
+  }
+}
